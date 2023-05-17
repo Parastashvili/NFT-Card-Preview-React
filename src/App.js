207a1080ea9fd652110d3aaaf8db8bc824844093
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-05-17 14:10:30 +0400

    first react project done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import logo from "./logo.svg";
-import Text from "./Text";
+import "./Reset.css";
 import "./App.css";
+import avatar from "./images/image-avatar.png";
+import ethereum from "./images/icon-ethereum.svg";
+import clock from "./images/icon-clock.svg";
 
 function App() {
   return (
-    <div className="container" style={styles.container}>
-      <h1>Hello world</h1>
-    </div>
+    <main className="container">
+      <section className="innerContainer">
+        <div>
+          <div id="nftimg"></div>
+          <div className="nft_dsc">
+            <h1 className="nft_name">Equilibrium #3429</h1>
+            <p className="nft_about">
+              Our Equilibrium collection promotes balance and calm.
+            </p>
+            <div className="priceandtime">
+              <div className="innerPriceandtime">
+                <img id="ethereum" src={ethereum} alt="ethereum-icon" />
+                <h4 className="pricetxt">0.041 ETH</h4>
+              </div>
+              <div className="innerPriceandtime">
+                <img id="clock" src={clock} alt="clock-icon" />
+                <h6 className="timeouttxt">3 days left</h6>
+              </div>
+            </div>
+          </div>
+          <div className="craetionSection">
+            <img id="avatar" src={avatar} alt="avatar" />
+            <p className="creation">Creation of</p>
+            <h2 className="creationName">Jules Wyvern</h2>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
 export default App;
-
-const styles = {
-  container: {
-    color: "red",
-
-  },
-};
--- src/Text.js
+++
@@ -1,5 +0,0 @@
-function Text() {
-  return <p>Hello this is my first components</p>;
-}
-
-export default Text;
